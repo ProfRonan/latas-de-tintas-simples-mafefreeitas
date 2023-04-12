0522bdf5950399723dace22fa1d4847f4c401e30
Fernanda <mfernandafg4@gmail.com>
2023-04-12 18:04:20 -0300

    pronto

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ metros_quadrados = float(metros_quadrados)
 
 # Coloque o código para resolver o problema nessa parte do programa
 
+litros = metros_quadrados / 3
+if litros % 18 > 0:
+    latas = (litros // 18) + 1
+else:
+    latas = litros // 18 
+total = latas * 80
+
 # As duas variáveis qtd_de_latas e valor_total devem guardar a resposta do problema
 # Troque os zeros pelos valores corretos.
-qtd_de_latas = 0
-valor_total = 0
+qtd_de_latas = int(latas)
+valor_total = int(total)
 
 print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
